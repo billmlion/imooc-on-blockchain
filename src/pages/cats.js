717d4ffbb4ae2837707ad037744e63c8d4201d01
bill <14253332@qq.com>
2019-12-16 23:07:45 +0800

    add cats's management with antd UI

---
+++ src/pages/cats.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Row, Col, Badge, Button, Switch, Table, Pagination,Divider, Tag } from 'antd';
+
+class Cats extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+           
+        }
+         
+    }
+
+    render() {
+      
+        return  <a>dd</a>
+    }
+}
+
+export default Cats
